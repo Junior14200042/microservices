1fe8205b44e3b6b2487abdcacf9af9ec993a1991
Junior <junior14200042@gmail.com>
2025-04-05 12:56:54 -0500

    Se agregó API GATEWAY, se configuró rutas y controladores

--- hotel-service/src/main/java/com/devjr/hotel/hotel_service/controller/HotelController.java
+++ hotel-service/src/main/java/com/devjr/hotel/hotel_service/controller/HotelController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/hotel")
+@RequestMapping("api/v1/hoteles")
 public class HotelController {
 
     private final IHotelService hotelService;
